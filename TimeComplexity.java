fb3e43024a1dda8581348198b3907b0e95a8561c
Rakhi Sharma <144309006+Sharma-Rakhi1@users.noreply.github.com>
2025-09-18 22:47:09 +0530

    Add files via upload

---
+++ TimeComplexity.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class TimeComplexity{
+    public static void main(String[] args){
+        double now = System.currentTimeMillis();
+        TimeComplexity demo = new TimeComplexity();
+        System.out.println(demo.findSum(99999));
+        System.out.println("Time Taken- "+(System.currentTimeMillis()-now)+"millisecs.");
+    }
+
+    // public int findSum(int n){
+    //     return n*(n+1)/2;
+    // }
+
+    public int findSum(int n){
+        int sum =0;
+        for(int i =0; i<=n; i++){
+            sum+=i;
+        }
+        return sum;
+    }
+}
