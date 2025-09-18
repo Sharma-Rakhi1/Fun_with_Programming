b0d3498c5a73ef323cd90f2de83dcf0be3e6f95a
Rakhi Sharma <144309006+Sharma-Rakhi1@users.noreply.github.com>
2025-09-18 22:55:37 +0530

    Update TimeComplexity.java

--- TimeComplexity.java
+++ TimeComplexity.java
@@ -3,9 +3,10 @@ public class TimeComplexity{
     public static void main(String[] args){
         double now = System.currentTimeMillis();
         TimeComplexity demo = new TimeComplexity();
-        System.out.println(demo.findSum(99999));
+        System.out.println(demo.findSum(99999)); // EDIT THIS LINE AND REPLACE FOR CALLING YOUR FUNCTION
         System.out.println("Time Taken- "+(System.currentTimeMillis()-now)+"millisecs.");
     }
+    // TRY OUT WITH YOUR FUNCTIONS BY PUTTING HERE
 
     // public int findSum(int n){
     //     return n*(n+1)/2;
@@ -18,4 +19,5 @@ public class TimeComplexity{
         }
         return sum;
     }
+
 }
